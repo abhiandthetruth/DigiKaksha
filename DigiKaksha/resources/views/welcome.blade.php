8a2ad8e868936be010a4c908819b45050b3f85d5
Manthan Surkar <manthan.surkar@gmail.com>
2020-05-02 22:17:34 +0530

    front-page

--- DigiKaksha/resources/views/welcome.blade.php
+++ DigiKaksha/resources/views/welcome.blade.php
@@ -1,4 +1,206 @@
 <!DOCTYPE html>
+<html>
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="Start your development with a Dashboard for Bootstrap 4.">
+  <meta name="author" content="Creative Tim">
+  <title>Digikaksha - A digital world</title>
+  <link rel="icon" href="assets/img/brand/favicon.png" type="image/png">
+  <!-- Fonts -->
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700">
+  <!-- Icons -->
+  <link rel="stylesheet" href="assets/vendor/nucleo/css/nucleo.css" type="text/css">
+  <link rel="stylesheet" href="assets/vendor/%40fortawesome/fontawesome-free/css/all.min.css" type="text/css">
+  <!-- Page plugins -->
+  <!-- Argon CSS -->
+  <link rel="stylesheet" href="assets/css/argon.min5438.css?v=1.2.0" type="text/css">
+</head>
+
+<body>
+  <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-NKDMSK6" height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+  <nav id="navbar-main" class="navbar navbar-horizontal navbar-main navbar-expand-lg navbar-dark bg-primary">
+    <div class="container">
+      <a class="navbar-brand" style="font-size: 200%; color:white;">
+         DigiKaksha
+      </a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbar-collapse" aria-controls="navbar-collapse" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="navbar-collapse navbar-custom-collapse collapse" id="navbar-collapse">
+        <div class="navbar-collapse-header">
+          <div class="row">
+            <div class="col-6 collapse-brand">
+             DigiKaksha
+            </div>
+            <div class="col-6 collapse-close">
+              <button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#navbar-collapse" aria-controls="navbar-collapse" aria-expanded="false" aria-label="Toggle navigation">
+                <span></span>
+                <span></span>
+              </button>
+            </div>
+          </div>
+        </div>
+        <ul class="navbar-nav mr-auto">
+        </ul>
+        <hr class="d-lg-none" />
+        <ul class="navbar-nav align-items-lg-center ml-lg-auto">
+          <li class="nav-item d-none d-lg-block ml-lg-4">
+            <a href="" target="_blank" class="btn btn-neutral btn-icon">
+              <span class="btn-inner--icon">
+                <i class="fas fa-school mr-2"></i>
+              </span>
+              <span class="nav-link-inner--text">Made for IIITA</span>
+            </a>
+            @if (Route::has('login'))
+                @auth
+                <a href="/home" target="_blank" class="btn btn-neutral btn-icon">
+                    <span class="btn-inner--icon">
+                      <i class="fas fa-home mr-2"></i>
+                    </span>
+                    <span class="nav-link-inner--text">Home</span>
+                  </a>
+               
+                @else
+                <a href="/login" target="_blank" class="btn btn-neutral btn-icon">
+                    <span class="btn-inner--icon">
+                      <i class="fas fa-user mr-2"></i>
+                    </span>
+                    <span class="nav-link-inner--text">Login</span>
+                  </a>
+                @endauth
+            </div>
+          @endif
+
+         
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+  <!-- Main content -->
+  <div class="main-content">
+    <!-- Header -->
+    <div class="header bg-primary pt-5 pb-7">
+      <div class="container">
+        <div class="header-body">
+          <div class="row align-items-center">
+            <div class="col-lg-6">
+              <div class="pr-5">
+                <h1 class="display-2 text-white font-weight-bold mb-0">A digital world</h1>
+                <h2 class="display-4 text-white font-weight-light">Please read the below para for sample login details</h2>
+                <p class="text-white mt-4">To test the application use the following sample details,<b> Username: student/admin/teacher Password:password</b>. Different type of user have different functions.</p>
+                <div class="mt-5">
+                    @if (Route::has('login'))
+                <div class="top-right links">
+                    @auth
+                        <a class="btn btn-neutral my-2" href="{{ url('/home') }}">Home</a>
+                    @else
+                        <a class="btn btn-neutral my-2" href="{{ route('login') }}">Login</a>
+
+                    @endauth
+                </div>
+            @endif
+    
+            
+                </div>
+              </div>
+            </div>
+            <div class="col-lg-6">
+              <div class="row pt-5">
+                <div class="col-md-6">
+                  <div class="card">
+                    <div class="card-body">
+                      <div class="icon icon-shape bg-gradient-red text-white rounded-circle shadow mb-4">
+                        <i class="ni ni-active-40"></i>
+                      </div>
+                      <h5 class="h3">Classrooms and Courses</h5>
+                      <p style="font-size:80%; ">Teacher can manage multiple courses,classroom, grading with instant feedback to students.</p>
+                    </div>
+                  </div>
+                  <div class="card">
+                    <div class="card-body">
+                      <div class="icon icon-shape bg-gradient-info text-white rounded-circle shadow mb-4">
+                        <i class="ni ni-active-40"></i>
+                      </div>
+                      <h5 class="h3">Announcements</h5>
+                      <p style="font-size:80%; ">Fully integrated Announcement feature to ease life of teacher and student.</p>
+                    </div>
+                  </div>
+                </div>
+                <div class="col-md-6 pt-lg-5 pt-4">
+                  <div class="card mb-4">
+                    <div class="card-body">
+                      <div class="icon icon-shape bg-gradient-success text-white rounded-circle shadow mb-4">
+                        <i class="ni ni-active-40"></i>
+                      </div>
+                      <h5 class="h3">Attendance</h5>
+                      <p style="font-size:80%;">Helps you maintain your attendance and helps you in skipping classes. Helps teacher/tutors Easily take and record attendance.</p>
+                    </div>
+                  </div>
+                  <div class="card mb-4">
+                    <div class="card-body">
+                      <div class="icon icon-shape bg-gradient-warning text-white rounded-circle shadow mb-4">
+                        <i class="ni ni-active-40"></i>
+                      </div>
+                      <h5 class="h3">Assignments</h5>
+                      <p style="font-size:80%;"> You will love how easy it is for teachers to keep track of assignments, and for students to submit and get them graded.</p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="separator separator-bottom separator-skew zindex-100">
+        <svg x="0" y="0" viewBox="0 0 2560 100" preserveAspectRatio="none" version="1.1" xmlns="http://www.w3.org/2000/svg">
+          <polygon class="fill-default" points="2560 0 2560 100 0 100"></polygon>
+        </svg>
+      </div>
+    </div>
+    <section class="py-6 pb-9 bg-default">
+      <div class="container">
+        <div class="row justify-content-center text-center">
+          <div class="col-md-8">
+            <h2 class="display-3 text-white">SOE Project - Group Gamma</h3>
+              <p class="lead text-white">
+           This web application is made as our software engineering project, the team Gamma(IIT2018040,43,80,95) has put a lot of efforts, this is a fully functional application which some exciting features :)
+              </p>
+          </div>
+        </div>
+      </div>
+    </section>
+    
+  </div>
+  <!-- Footer -->
+  <footer class="py-5" id="footer-main">
+    <div class="container">
+      <div class="row align-items-center justify-content-xl-between">
+        <div class="col-xl-6">
+          <div class="copyright text-center text-xl-left text-muted">
+            &copy; 2020 DigiKaksha</a>
+          </div>
+        </div>
+        <div class="col-xl-6">
+        </div>
+      </div>
+    </div>
+  </footer>
+
+  <script src="assets/vendor/jquery/dist/jquery.min.js"></script>
+  <script src="assets/vendor/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
+  <script src="assets/vendor/js-cookie/js.cookie.js"></script>
+  <script src="assets/vendor/jquery.scrollbar/jquery.scrollbar.min.js"></script>
+  <script src="assets/vendor/jquery-scroll-lock/dist/jquery-scrollLock.min.js"></script>
+  <script src="assets/vendor/onscreen/dist/on-screen.umd.min.js"></script>
+  <script src="assets/js/argon.min5438.js?v=1.2.0"></script>
+
+</body>
+</html>
+{{-- <!--
+<!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
         <meta charset="utf-8">
@@ -81,20 +283,14 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    DigiKaksha
                 </div>
 
                 <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://vapor.laravel.com">Vapor</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+                    <a href="https://laravel.com/docs">a Digital World with Classroom, Courses, Attendance, Assignments, teachers, you and me ;)</a>
+               
                 </div>
             </div>
         </div>
     </body>
-</html>
+</html> --}}
