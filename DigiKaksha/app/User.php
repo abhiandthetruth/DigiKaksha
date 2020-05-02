fee87872605e2dba750f2a273f2a5ad6d1247b4e
abhiandthetruth <abhiandthetruth@gmail.com>
2020-05-02 23:21:26 +0530

    Dashboards fixed

--- DigiKaksha/app/User.php
+++ DigiKaksha/app/User.php
@@ -73,4 +73,62 @@ class User extends Authenticatable
         foreach($this->submissions as $submission) if($submission->announcement->course_id==$course->id) if($submission->grade!=NULL)$i+=1;
         return $i;
     }
+    public function getTotalCourses(){
+        $i=0;
+        if($this->user_level==1){
+            foreach($this->groups as $group){
+                $i+=count($group->courses);
+            }
+        }
+        elseif($this->user_level==2) $i = count($this->courses);
+        else $i = count(\App\Course::all());
+        return $i;
+    }
+    public function averageMarks(){
+        $i=0;
+        $j=0;
+        foreach($this->groups as $group){
+            foreach($group->courses as $course){
+                $max = $this->getMaxScore($course);
+                if($max!=0){
+                    $i+=$this->getScore($course)/$max;
+                    $j++;
+                }
+            }
+        }
+        $r = $i*100/$j;
+        return $r;
+    }
+    public function getTotalClasses()
+    {
+        $i=0;
+        if($this->user_level==1){
+            $i = count($this->groups);
+        }
+        elseif($this->user_level==2){
+            foreach($this->courses as $course){
+                $i+=count($course->groups);
+            }
+        }
+        else $i = count(\App\Group::all());
+        return $i;
+    }
+    public function getTotalStudents()
+    {
+        $i=0;
+        $users = \App\User::all();
+        foreach($users as $user){
+            if($user->user_level==1) $i++;
+        }
+        return $i;
+    }
+    public function getTotalInstructors()
+    {
+        $i=0;
+        $users = \App\User::all();
+        foreach($users as $user){
+            if($user->user_level==2) $i++;
+        }
+        return $i;
+    }
 }
--- DigiKaksha/resources/views/home.blade.php
+++ DigiKaksha/resources/views/home.blade.php
@@ -350,8 +350,7 @@
               <nav aria-label="breadcrumb" class="d-none d-md-inline-block ml-md-4">
                 <ol class="breadcrumb breadcrumb-links breadcrumb-dark">
                   <li class="breadcrumb-item"><a href="#"><i class="fas fa-home"></i></a></li>
-                  <li class="breadcrumb-item"><a href="#">Dashboards</a></li>
-                  <li class="breadcrumb-item active" aria-current="page">Default</li>
+                  <li class="breadcrumb-item"><a href="#">Dashboard</a></li>
                 </ol>
               </nav>
             </div>
@@ -374,8 +373,13 @@
                 <div class="card-body">
                   <div class="row">
                     <div class="col">
-                      <h5 class="card-title text-uppercase text-muted mb-0">Total traffic</h5>
-                      <span class="h2 font-weight-bold mb-0">350,897</span>
+                      @if(Auth::user()->user_level<3)
+                      <h5 class="card-title text-uppercase text-muted mb-0">Your ID</h5>
+                      <span class="h2 font-weight-bold mb-0">{{Auth::user()->roll_no}}</span>
+                      @else
+                      <h5 class="card-title text-uppercase text-muted mb-0">Instructors</h5>
+                      <span class="h2 font-weight-bold mb-0">{{Auth::user()->getTotalInstructors()}}</span>
+                      @endif
                     </div>
                     <div class="col-auto">
                       <div class="icon icon-shape bg-gradient-red text-white rounded-circle shadow">
@@ -383,10 +387,6 @@
                       </div>
                     </div>
                   </div>
-                  <p class="mt-3 mb-0 text-sm">
-                    <span class="text-success mr-2"><i class="fa fa-arrow-up"></i> 3.48%</span>
-                    <span class="text-nowrap">Since last month</span>
-                  </p>
                 </div>
               </div>
             </div>
@@ -396,8 +396,13 @@
                 <div class="card-body">
                   <div class="row">
                     <div class="col">
-                      <h5 class="card-title text-uppercase text-muted mb-0">New users</h5>
-                      <span class="h2 font-weight-bold mb-0">2,356</span>
+                      @if(Auth::user()->user_level==1)
+                        <h5 class="card-title text-uppercase text-muted mb-0">Average Marks</h5>
+                        <span class="h2 font-weight-bold mb-0">{{Auth::user()->averageMarks()}}%</span>
+                      @else
+                        <h5 class="card-title text-uppercase text-muted mb-0">Students</h5>
+                        <span class="h2 font-weight-bold mb-0">{{Auth::user()->getTotalStudents()}}</span>
+                      @endif
                     </div>
                     <div class="col-auto">
                       <div class="icon icon-shape bg-gradient-orange text-white rounded-circle shadow">
@@ -405,10 +410,6 @@
                       </div>
                     </div>
                   </div>
-                  <p class="mt-3 mb-0 text-sm">
-                    <span class="text-success mr-2"><i class="fa fa-arrow-up"></i> 3.48%</span>
-                    <span class="text-nowrap">Since last month</span>
-                  </p>
                 </div>
               </div>
             </div>
@@ -418,8 +419,8 @@
                 <div class="card-body">
                   <div class="row">
                     <div class="col">
-                      <h5 class="card-title text-uppercase text-muted mb-0">Sales</h5>
-                      <span class="h2 font-weight-bold mb-0">924</span>
+                      <h5 class="card-title text-uppercase text-muted mb-0">Courses</h5>
+                    <span class="h2 font-weight-bold mb-0">{{Auth::user()->getTotalCourses()}}</span>
                     </div>
                     <div class="col-auto">
                       <div class="icon icon-shape bg-gradient-green text-white rounded-circle shadow">
@@ -427,10 +428,6 @@
                       </div>
                     </div>
                   </div>
-                  <p class="mt-3 mb-0 text-sm">
-                    <span class="text-success mr-2"><i class="fa fa-arrow-up"></i> 3.48%</span>
-                    <span class="text-nowrap">Since last month</span>
-                  </p>
                 </div>
               </div>
             </div>
@@ -440,8 +437,8 @@
                 <div class="card-body">
                   <div class="row">
                     <div class="col">
-                      <h5 class="card-title text-uppercase text-muted mb-0">Performance</h5>
-                      <span class="h2 font-weight-bold mb-0">49,65%</span>
+                      <h5 class="card-title text-uppercase text-muted mb-0">Classes</h5>
+                      <span class="h2 font-weight-bold mb-0">{{Auth::user()->getTotalClasses()}}</span>
                     </div>
                     <div class="col-auto">
                       <div class="icon icon-shape bg-gradient-info text-white rounded-circle shadow">
@@ -449,10 +446,6 @@
                       </div>
                     </div>
                   </div>
-                  <p class="mt-3 mb-0 text-sm">
-                    <span class="text-success mr-2"><i class="fa fa-arrow-up"></i> 3.48%</span>
-                    <span class="text-nowrap">Since last month</span>
-                  </p>
                 </div>
               </div>
             </div>
@@ -491,9 +484,6 @@
                               </h4>
                               <span class="text-success">●</span>
                               <small>Online</small>
-                          </div>
-                          <div class="col-auto">
-                              <button type="button" class="btn btn-sm btn-primary">Add</button>
                           </div>
                           </div>
                       </li>
